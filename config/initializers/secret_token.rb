5b454bc7da7a1e57153b0405261498a356f4dd0e
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-30 10:05:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billbo::Application.config.secret_token = '25314ae1ad7cbd8c872e5226659ea210df8665a7efa5b88a1e82ad2bcf4b52fa9a213e432ff89ff64cf95ddb19686483911196e3d75bed6d80b8ee53f38ff5a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Billbo::Application.config.session_store :cookie_store, key: '_Billbo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Billbo::Application.config.session_store :active_record_store
