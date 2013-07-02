d4eebfac30041d00ab8002226d90352976cee3a9
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-01 21:33:16 -0700

    add connections controller, create action

---
+++ app/controllers/connections_controller.rb
@@ -0,0 +1,8 @@
+class ConnectionsController < ApplicationController
+
+	def create
+		@connection = current_user.connections.build(params[:receiver_id])
+		@connection.save!
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Billbo::Application.routes.draw do
+	root :to => "root#root"
+
+	resources :connections, :only => [:create]
+
   resource :session, :only => [:create, :destroy, :new]
   resources :users, :only => [:create, :new]
-
-  root :to => "root#root"
+  
 end
