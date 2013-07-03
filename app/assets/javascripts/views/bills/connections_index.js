7bbd0aaceed633fd13bcc00f93d870e815d69673
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-03 09:57:25 -0700

    fixed bug that allowed adding duplicate connections to the page

--- app/assets/javascripts/views/bills/connections_index.js
+++ app/assets/javascripts/views/bills/connections_index.js
@@ -10,6 +10,7 @@ Billbo.Views.ConnectionsIndex = Backbone.View.extend({
     var that = this;
 
     that.listenTo(that.collection, "add", that.render);
+    that.listenTo(that.collection, "remove", that.render);
   },
 
   render: function () {
@@ -32,7 +33,12 @@ Billbo.Views.ConnectionsIndex = Backbone.View.extend({
 
     this.model.set(attrs);
     if (this.model.isNew()) {
-      this.collection.create(this.model);
+      this.collection.create(this.model, {
+        error: function () {
+          console.log("connection already exists");
+          that.collection.remove(that.model);
+        }
+      });
     }
   }
 
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -13,7 +13,14 @@ class Connection < ActiveRecord::Base
   attr_accessible :creator_id, :receiver_id
 
   validates :creator, :receiver, :presence => true
+  validate :creator_cannot_be_receiver
 
   belongs_to :creator, :class_name => "User"
   belongs_to :receiver, :class_name => "User"
+
+  def creator_cannot_be_receiver
+  	if creator_id == receiver_id
+  		errors[:receiver] << "must be a different user"
+  	end
+  end
 end
