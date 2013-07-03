310c06febb880e0516784fda3cdfbe1c423f116e
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-03 16:36:37 -0700

    inverse connections created

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -4,6 +4,11 @@ class ConnectionsController < ApplicationController
 		receiver = User.find_by_email(params[:email])
 		@connection = current_user.connections.build( :receiver_id => receiver.id)
 		@connection.save!
+		inverse_creator_id = @connection.receiver_id
+		inverse_receiver_id = @connection.creator_id
+		@inverse_connection = Connection.new( :creator_id => inverse_creator_id, 
+																					:receiver_id => inverse_receiver_id )
+		@inverse_connection.save!
 		render :json => @connection
 	end
 
