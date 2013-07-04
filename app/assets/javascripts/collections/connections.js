94bcf2f545c9201af6055d6ba6c1f39804bf51b1
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-03 17:25:05 -0700

    connections list sorts alphabetically

--- app/assets/javascripts/collections/connections.js
+++ app/assets/javascripts/collections/connections.js
@@ -1,6 +1,9 @@
 Billbo.Collections.Connections = Backbone.Collection.extend({
 
   model: Billbo.Models.Connection,
-  url: '/connections'
+  url: '/connections',
+  comparator: function (connection) {
+  	return connection.get('email');
+  }
 
 });
