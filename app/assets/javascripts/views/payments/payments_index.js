ce89cb9a96b2b5ccc0a589471375a0006e104500
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-07 12:06:56 -0700

    nicer payment delete links

--- app/assets/javascripts/views/payments/payments_index.js
+++ app/assets/javascripts/views/payments/payments_index.js
@@ -25,7 +25,7 @@ Billbo.Views.PaymentsIndex = Backbone.View.extend({
 
   delete: function (event) {
     event.preventDefault();
-    var id = $(event.target).attr('data-id');
+    var id = $(event.target).parent().attr('data-id');
     var payment = this.collection.get(id);
     payment.destroy({ wait: true});
   }
